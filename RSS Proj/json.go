96bdd7840b069ac26e70e0bc46762d7becb73dc4
1Shubham7 <shubhammahar1306@gmail.com>
2023-06-24 15:02:55 +0530

    ==

--- RSS Proj/json.go
+++ RSS Proj/json.go
@@ -3,8 +3,14 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"log"
 )
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}){
 	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Failed ot marshal JSON response: %v", payload)
+		w.WriteHeader(500)
+		return
+	}
 }
